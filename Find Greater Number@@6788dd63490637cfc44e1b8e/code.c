f01796eb824f4f6c89405aeb9b86effb7d0ba5c5
Niharika Khosla <niharikakhosla20@gmail.com>
2025-02-12 11:26:33 +0530

    Solution to Find Greater Number

---
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -0,0 +1,16 @@
+// Your code here...
+#include <stdio.h>
+
+int num1,num2;
+  
+int main() {
+    scanf("%d %d",&num1,&num2);
+    if(num1%2==0){
+        printf("Even");
+    }
+    else{
+        printf("Odd");
+    }
+    
+    return 0;
+    }
