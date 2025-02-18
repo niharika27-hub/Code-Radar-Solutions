cdfd46148b4de5ac2489f2cee7615d433faa793a
Niharika Khosla <niharikakhosla20@gmail.com>
2025-02-18 10:34:46 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 int main(){
     char ch;
-    scanf("%d",&ch );
+    scanf("%c",&ch );
     if
         (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
         ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
